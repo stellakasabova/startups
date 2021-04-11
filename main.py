7f1444e7b94774030e4cd7776072801c80fdbff0
Kalin Asenov <72384461+Blepeian@users.noreply.github.com>
2021-04-11 23:14:05 +0300

    completed login and added registration

--- main.py
+++ main.py
@@ -1,32 +1,72 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'very secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-user = {"username": "abc", "password": "xyz"}  # temporary dictionary for testing until we add a db
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    education = db.Column(db.String(80), nullable=False)
+    about = db.Column(db.String(350), nullable=True)
+    position = db.Column(db.String(40), nullable=False)
 
 
 @app.route('/')
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        name = request.form.get('name')
+        age = request.form.get('age')
+        education = request.form.get('education')
+        position = request.form.get('position')
+        about = request.form.get('about')
+
+        new_user = Users(username=username, password=password, name=name, age=age, education=education,
+                         position=position, about=about)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/homepage')  # will redirect it to a different page at a later date
+        except:
+            return "<h1>Something went wrong while registering</h1>"
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == user['username'] and password == user['password']:
+
+        user = Users.query.filter_by(username=username, password=password).first()
+
+        if not user:
+            return "<h1>Wrong username or password</h1>"
+        else:
             session['user'] = username
-            return redirect('/homepage')
+            return redirect('/homepage')  # will redirect it to a different page at a later date
 
-        return "<h1>Wrong username or password</h1>"
-
-    return render_template('login.html')
-
-
-@app.route('/homepage')
-def homepage():
-    if 'user' in session and session['user'] == user['username']:
-        return '<h1>Homepage</h1>'  # this is a placeholder
-
-    return '<h1>You are not logged in.</h1>'
+    if request.method == 'GET':
+        return render_template('login.html')
 
 
 @app.route('/logout')
