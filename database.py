a2dd0d6f7a3a26cdf25d399850051b5988d8266d
Kalin Asenov <72384461+Blepeian@users.noreply.github.com>
2021-05-27 23:58:01 +0300

    put database models in a separate file

---
+++ database.py
@@ -0,0 +1,20 @@
+from main import db
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    education = db.Column(db.String(80), nullable=False)
+    about = db.Column(db.String(350), nullable=True)
+    position = db.Column(db.String(40), nullable=False)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(2000), nullable=False)
+    rating = db.Column(db.Integer, nullable=False, default=0)
